ebafa8f382ae96f1d86b7d3c79c090ca666dc8c4
Nicholas Dujay <nickdujay@gmail.com>
2018-03-24 11:44:33 -0400

    ok cool, switch to parcel

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <div>
+        <p>hello world</p>
+        <p>nick is wedding pirave on july 15, 2018</p>
+        <iframe
+            width="600"
+            height="450"
+            frameBorder="0"
+            style={{ border: '0' }}
+            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ3TuAfPvT1IkRR2qwJvcxUpQ&key=AIzaSyByKiSswQhQtBA-amrIC7Oqvv2vJuUeMy4"
+            allowFullScreen
+        />
+    </div>
+)
+
+render(
+    <App />,
+    document.getElementById('react-root')
+)
